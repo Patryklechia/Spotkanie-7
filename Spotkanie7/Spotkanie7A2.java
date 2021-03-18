328a0be9e88eae066d740fc32c498395dbdb47b4
Patryklechia <79514897+Patryklechia@users.noreply.github.com>
2021-03-18 22:20:46 +0100

    Add files via upload

---
+++ Spotkanie7/Spotkanie7A2.java
@@ -0,0 +1,22 @@
+package Spotkanie7;
+public class Spotkanie7A2 {
+    public static void main(String[] args) {
+        int[][][][] array = new int[3][3][3][3];
+        int value2 = 0;
+        for (
+                int i = 0;
+                i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int n = 0; n < 3; n++) {
+                    for (int m = 0; m < 3; m++) {
+                        array[i][j][n][m] = value2;
+                        value2 += 3;
+                        System.out.print("[" + i + "][" + j + "][" + n + "][" + m + "] = " + array[i][j][n][m] + "\t");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
